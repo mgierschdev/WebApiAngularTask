433db6af2ca8049c6112dc12234f1db78e7db5c9
pacmax2 <mgierschdev@gmail.com>
2023-02-06 14:16:13 -0400

    Adding the user validator

---
+++ Backend/UserBackend/Domain/Validator/UserValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace Domain.Validator;
+using System;
+using API.Domain.User;
+using FluentValidation;
+
+
+// All the buss logic on the Domain as specified in the DDD docs, easier to maintain and test
+
+public class UserValidator : AbstractValidator<APIUser>
+{
+    public UserValidator()
+    {
+        RuleFor(APIUser => APIUser.FirstName).NotNull();
+        RuleFor(APIUser => APIUser.Email).NotNull();
+        RuleFor(APIUser => APIUser.PhoneNumber).NotNull();
+    }
+}
