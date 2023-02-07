e86de7e10d46bc0d10d4a419a09ad930e30948f1
pacmax2 <mgierschdev@gmail.com>
2023-02-07 14:25:58 -0400

    re-using the same view for the users and posts tables

--- Frontend/src/app/table-component/table-component.component.ts
+++ Frontend/src/app/table-component/table-component.component.ts
@@ -8,13 +8,45 @@ import { User, Post } from '../app.component';
 })
 
 export class TableComponentComponent {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-  @Input() UserList : User[];
-  @Input() PostsList : Post[];
-  dataSource : any;
+  @Input() UserList: User[];
+  @Input() PostsList: Post[];
+  displayedColumnsUser: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumnsPost: string[] = ['position', 'name', 'weight', 'symbol'];
+  UserTemplate: boolean = true;
 
-  public constructor(){
-    this.UserList = [];
-    this.PostsList = [];
+  public constructor() {
+    // To be loaded from the backend
+    this.UserList = this.USER_DATA;
+    this.PostsList = this.POST_DATA;
   }
+
+  public SetUserTemplate(val: boolean) {
+    this.UserTemplate = val;
+  }
+
+  USER_DATA: User[] = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  ];
+
+  POST_DATA: User[] = [
+    { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+    { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+    { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+    { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+    { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+    { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+    { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+    { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+    { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+    { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+  ];
 }
