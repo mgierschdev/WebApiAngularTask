01c2f211eccf33998d6ef4320620824e91a016bb
pacmax2 <mgierschdev@gmail.com>
2023-02-07 14:27:13 -0400

    re-formating

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 export interface Tile {
   color: string;
   cols: number;
--- Frontend/src/app/grid-component/grid-component.component.ts
+++ Frontend/src/app/grid-component/grid-component.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { User } from '../app.component';
 
 @Component({
   selector: 'app-grid-component',
