0cc22f91e733cdf4b776978639e003153297a38d
pacmax2 <mgierschdev@gmail.com>
2023-02-08 11:42:19 -0400

    Adding final test users

--- Backend/UserBackend/Infrastructure/Util/Util.cs
+++ Backend/UserBackend/Infrastructure/Util/Util.cs
@@ -9,44 +9,87 @@ public static class Util
     {
         return new List<User>()
             {
-                new User {
-                    FirstName = "FirstName",
-                    LastName =  "LastName",
-                    Email = "email@provider.com",
+
+            new User {
+                    FirstName = "TestUserFirstName",
+                    LastName =  "TestUserSecondName",
+                    Email = "email1@provider.com",
                     PhoneNumber = "+4312345678",
                     ProfileViews = 0,
                     CreationTime = DateTime.Now,
                     LastLoginTime = DateTime.Now,
-                    Posts = new List<Post> {new Post {Title = "Title", Content = "Content of the post", CreationTime = DateTime.Now}}},
-                new User {
-                    FirstName = "FirstName",
-                    LastName =  "LastName",
-                    Email = "email@provider.com",
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 1", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 2", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 3", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 4", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
+            new User {
+                    FirstName = "2TestUserFirstName",
+                    LastName =  "2TestUserSecondName",
+                    Email = "email2@provider.com",
                     PhoneNumber = "+4312345678",
                     ProfileViews = 0,
                     CreationTime = DateTime.Now,
                     LastLoginTime = DateTime.Now,
-                    Posts = new List<Post> {new Post {Title = "Title", Content = "Content of the post", CreationTime = DateTime.Now}}},
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 5", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 6", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
+            new User {
+                    FirstName = "3TestUserFirstName",
+                    LastName =  "3TestUserSecondName",
+                    Email = "email3@provider.com",
+                    PhoneNumber = "+4312345678",
+                    ProfileViews = 0,
+                    CreationTime = DateTime.Now,
+                    LastLoginTime = DateTime.Now,
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 7", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
+            new User {
+                    FirstName = "4TestUserFirstName",
+                    LastName =  "4TestUserSecondName",
+                    Email = "email4@provider.com",
+                    PhoneNumber = "+4312345678",
+                    ProfileViews = 0,
+                    CreationTime = DateTime.Now,
+                    LastLoginTime = DateTime.Now,
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 8", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 9", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 10", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 11", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
 
                 new User {
-                    FirstName = "FirstName",
-                    LastName =  "LastName",
-                    Email = "email@provider.com",
+                    FirstName = "5TestUserFirstName",
+                    LastName =  "5TestUserSecondName",
+                    Email = "email5@provider.com",
                     PhoneNumber = "+4312345678",
                     ProfileViews = 0,
                     CreationTime = DateTime.Now,
                     LastLoginTime = DateTime.Now,
-                    Posts = new List<Post> {new Post {Title = "Title", Content = "Content of the post", CreationTime = DateTime.Now}}},
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 12", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
 
-                new User {
-                    FirstName = "FirstName",
-                    LastName =  "LastName",
-                    Email = "email@provider.com",
+            new User {
+                    FirstName = "6TestUserFirstName",
+                    LastName =  "6TestUserSecondName",
+                    Email = "email6@provider.com",
                     PhoneNumber = "+4312345678",
                     ProfileViews = 0,
                     CreationTime = DateTime.Now,
                     LastLoginTime = DateTime.Now,
-                    Posts = new List<Post> {new Post {Title = "Title", Content = "Content of the post", CreationTime = DateTime.Now}}}
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 13", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 14", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
             };
     }
 }
