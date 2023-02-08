e0cfec40b39f557d0a73294fd5b439dd7a969281
pacmax2 <mgierschdev@gmail.com>
2023-02-08 11:51:46 -0400

    Adding more test users

--- Backend/UserBackend/Infrastructure/Util/Util.cs
+++ Backend/UserBackend/Infrastructure/Util/Util.cs
@@ -90,6 +90,71 @@ public static class Util
                         new Post {Title = "Post Title 14", Content = "Content of the post", CreationTime = DateTime.Now},
                     }},
 
+            new User {
+                    FirstName = "7TestUserFirstName",
+                    LastName =  "7TestUserSecondName",
+                    Email = "email7@provider.com",
+                    PhoneNumber = "+4312345678",
+                    ProfileViews = 0,
+                    CreationTime = DateTime.Now,
+                    LastLoginTime = DateTime.Now,
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 13", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 14", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
+            new User {
+                    FirstName = "8TestUserFirstName",
+                    LastName =  "8TestUserSecondName",
+                    Email = "email8@provider.com",
+                    PhoneNumber = "+4312345678",
+                    ProfileViews = 0,
+                    CreationTime = DateTime.Now,
+                    LastLoginTime = DateTime.Now,
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 15", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 16", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
+            new User {
+                    FirstName = "9TestUserFirstName",
+                    LastName =  "9TestUserSecondName",
+                    Email = "email9@provider.com",
+                    PhoneNumber = "+4312345678",
+                    ProfileViews = 0,
+                    CreationTime = DateTime.Now,
+                    LastLoginTime = DateTime.Now,
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 17", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 18", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
+            new User {
+                    FirstName = "10TestUserFirstName",
+                    LastName =  "10TestUserSecondName",
+                    Email = "email10@provider.com",
+                    PhoneNumber = "+4312345678",
+                    ProfileViews = 0,
+                    CreationTime = DateTime.Now,
+                    LastLoginTime = DateTime.Now,
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 19", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 20", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
+            new User {
+                    FirstName = "11TestUserFirstName",
+                    LastName =  "11TestUserSecondName",
+                    Email = "email11@provider.com",
+                    PhoneNumber = "+4312345678",
+                    ProfileViews = 0,
+                    CreationTime = DateTime.Now,
+                    LastLoginTime = DateTime.Now,
+                    Posts = new List<Post> {
+                        new Post {Title = "Post Title 21", Content = "Content of the post", CreationTime = DateTime.Now},
+                        new Post {Title = "Post Title 22", Content = "Content of the post", CreationTime = DateTime.Now},
+                    }},
+
             };
     }
 }
